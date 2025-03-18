507214471590a291c70130874819486682d34dea
Augustin Morval <augustin.morval.pro@gmail.com>
2025-03-18 22:20:49 +0100

    flask app design

--- flask_app.py
+++ flask_app.py
@@ -4,30 +4,38 @@ import pickle
 import numpy as np
 import requests
 from loguru import logger
+import os
+import yaml
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 BACKEND_URL='https://fast-317097237537.europe-west1.run.app'
 
+with open('static/microorganism.yaml', 'r') as file:
+    microorganism_data = yaml.safe_load(file)
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Prédiction via formulaire
 @app.route('/predict', methods=['POST'])
 def predict_microbe():
-    # Prédiction avec le modèle
+    
     r = requests.post(f'{BACKEND_URL}/predict', json=request.form)
     
-    # Chercher le microbe associé et son image
     predicted_microbe = r.json()['class_predicted'][0]
     
-    # Récupérer le chemin de l'image et générer l'URL
     image_path = url_for('static', filename='img/' + predicted_microbe + '.png')
-        
-    # Retourner la page de résultat avec le microbe et l'image
-    return render_template('results.html', microbe=predicted_microbe, image_path=image_path)
+    
+    microbe_info = microorganism_data['microorganisms'].get(predicted_microbe, {})
+    
+    return render_template('results.html', 
+                         microbe=predicted_microbe, 
+                         image_path=image_path,
+                         description=microbe_info.get('description', ''),
+                         type=microbe_info.get('type', ''),
+                         habitat=microbe_info.get('habitat', ''))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
